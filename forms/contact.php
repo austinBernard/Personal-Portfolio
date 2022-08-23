458fd707884198429c7a3813cd633d78e4722502
Austin Bernard <109118567+austinBernard@users.noreply.github.com>
2022-08-23 03:18:14 -0500

    Updates 8/23/2022

--- forms/contact.php
+++ forms/contact.php
@@ -2,11 +2,23 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'austinbernardSE@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  if(isset($_POST['submit'])){
+    $to = "austinbernardSE@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
